858e8c7b96d18d3aef420260a259aac64ea1c883
singchan <billy321@im-paris.fr>
2017-05-30 20:47:57 +0200

    Create creative/js/script.js

---
+++ creative/js/script.js
@@ -0,0 +1,9 @@
+
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
+            });
+
+        });
+    
